1f57b99ec2599b910cddaca5189bc15499df47b9
Petri Silen <petri.silen@nokia.com>
2022-12-08 11:25:48 +0200

    Added chapter 1

---
+++ chapter-1/spring-example/src/main/java/com/example/demo/SalesItemService.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public interface SalesItemService {
+    SalesItem createSalesItem(SalesItemArg salesItemArg);
+    SalesItem getSalesItemById(Long id);
+
+    Iterable<SalesItem> getSalesItemsByUserAccountId(
+            Long userAccountId
+    );
+
+    Iterable<SalesItem> getSalesItems();
+    void updateSalesItem(Long id, SalesItemArg salesItemArg);
+    void deleteSalesItemById(Long id);
+    void deleteSalesItems();
+}
